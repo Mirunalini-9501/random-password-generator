2b2d880bb3330bdc3f8e9dd14d95cd5283b44fb5
ToastCoder <vicky.pcbasic@gmail.com>
2020-09-28 20:04:52 +0530

    Minor changes

--- randompassword.py
+++ randompassword.py
@@ -9,14 +9,16 @@ def get_random_alphanumeric_string(length):
     print("Random alphanumeric String is:", result_str)
 
 # MAIN FUNCTION
+print("---------------------------------------------------------")
 print("Welcome to Random password generator!")
 print("Enter length as -1 to exit")
+print("---------------------------------------------------------")
 while True:
-    digits = int(input("Enter the length for the random password (Minimum of 8 digits): "))
+    digits = int(input("\nEnter the length for the random password (Minimum of 8 digits): "))
     if digits>=8:
         get_random_alphanumeric_string(digits)
     elif digits>-1 and digits <8:
-        print("Minimum number of length for password is 8. Please try again!")
+        print("\nMinimum number of length for password is 8. Please try again!")
     elif digits==-1:
         break
 
