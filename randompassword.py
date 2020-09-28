bf1e026262b6b8e04b98c6459061a95172d29eb9
ToastCoder <vicky.pcbasic@gmail.com>
2020-09-28 21:30:55 +0530

    Minor error fixes

--- randompassword.py
+++ randompassword.py
@@ -17,7 +17,7 @@ while True:
     digits = int(input("\nEnter the length for the random password (Minimum of 8 digits): "))
     if digits>=8:
         print("\nGenerated Random password is:",pass_generator(digits))
-    elif digits>-1 and digits <8:
+    elif digits!=-1 and digits <8:
         print("\nMinimum number of length for password is 8. Please try again!")
     elif digits==-1:
         break
